b5508a4ba5f247daeebd8329e79ee36fa792d31d
lkysammi <kleun136@my.centennialcollege.ca>
2023-05-10 21:54:46 -0400

    module

---
+++ js/lib.js
@@ -0,0 +1,11 @@
+export function squareValue(n){
+    return n * n;
+}
+
+export function halfOf(x){
+    return x / 2;
+}
+
+export function yourName(){
+
+}
---
+++ js/main.js
@@ -0,0 +1,5 @@
+import { squareValue } from "lib.js"
+console.log(squareValue(11));
+console.log(halfOf(10));
+doSomething();
+console.log(yourName);
